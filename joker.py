3722108c26b6d850b22c5fd83f891e09e2b221e0
JokerWare <steew0x8@protonmail.com>
2020-02-04 17:29:52 +0100

    Various fixes

--- joker.py
+++ joker.py
@@ -28,7 +28,7 @@ async def startClient():
         
         await bot_thread.client.start(pr_l["token"], bot=False)
     except Exception as e:
-        if("Improper token" in e):
+        if(e.__class__.__name__ == "LoginFailure"):
                 print("Something went wrong with the token. If this is the first time you use this bot, please go to http://localhost:5555 and edit your preferences.")
         else:
             print(e)
